aa6989ed7a10824cad65081fbb380a4886ee2883
Somar Romero <somar.dev@icloud.com>
2024-07-30 20:46:05 +0000

    feat: Update Generate PDF options in app.js
    The Generate PDF options in app.js have been updated to include a new field called "Template" with the type "string" and an interface of "input-code". This change allows users to input a template for generating PDFs with flow data.

--- src/app.js
+++ src/app.js
@@ -3,25 +3,16 @@ export default {
 	name: 'Generate PDF',
 	icon: 'picture_as_pdf',
 	description: 'Generate a pdf with flow data and the template.',
-	overview: ({ collection, url }) => [
-		{
-			label: '$t:collection',
-			text: collection,
-		},
-		{
-			label: 'Url',
-			text: url,
-		},
-	],
 	options: [
 		{
-			field: 'text',
-			name: 'Text',
+			field: 'template',
+			name: 'Template',
 			type: 'string',
 			meta: {
 				width: 'full',
-				interface: 'input',
+				interface: 'input-code',
 			},
 		},
+
 	],
 };
