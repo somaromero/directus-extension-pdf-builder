1510fa5e27cf0fdc58942a4fdd0f96695a873262
M4rz0 <37194776+HelloImZomi@users.noreply.github.com>
2024-08-12 16:15:07 -0700

    feat: Save the files on the File Library :sparkles:

--- src/api.js
+++ src/api.js
@@ -1,6 +1,73 @@
+import pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from './vfs_fonts';
+import {Readable} from 'stream';
+
+pdfMake.vfs = pdfFonts.vfs;
+
 export default {
-	id: 'operation-pdf-generator',
-	handler: ({ text }) => {
-		console.log(text);
-	},
+    id: 'operation-pdf-generator',
+    handler: async ({filename, folder, storage, template, fonts}, {services, database, accountability, getSchema}) => {
+        const {FilesService} = services;
+        const schema = await getSchema({database});
+        const filesService = new FilesService({
+            schema: schema,
+            accountability: accountability,
+        });
+        let fileId = null;
+
+        try {
+            pdfMake.vfs = fonts;
+            const pdfDocGenerator = pdfMake.createPdf(template);
+
+            const buffer = await new Promise((resolve, reject) => {
+                pdfDocGenerator.getBuffer((buffer) => {
+                    resolve(buffer);
+                });
+            });
+
+            const pdfStream = bufferToReadable(buffer);
+            const data = {
+                title: filename,
+                filename_download: `${filename}.pdf`,
+                type: 'application/pdf',
+                storage: 'local',
+                folder: folder
+            };
+
+            try {
+                fileId = await filesService.uploadOne(pdfStream, data);
+            } catch (uploadError) {
+                console.error('Error uploading PDF:', uploadError);
+                return {
+                    success: false,
+                    message: "Error uploading PDF",
+                    error: uploadError.message
+                };
+            }
+        } catch (error) {
+            console.error('Error generating PDF:', error);
+            return {
+                success: false,
+                message: "Error generating PDF",
+                error: error.message
+            };
+        }
+
+        return {
+            success: true,
+            message: "File generated successfully",
+            data: {
+                uuid: fileId
+            }
+        };
+    },
 };
+
+function bufferToReadable(buffer) {
+    const readable = new Readable();
+    readable._read = () => {
+    };
+    readable.push(buffer);
+    readable.push(null);
+    return readable;
+}
--- src/app.js
+++ src/app.js
@@ -1,18 +1,107 @@
 export default {
-	id: 'operation-pdf-generator',
-	name: 'Generate PDF',
-	icon: 'picture_as_pdf',
-	description: 'Generate a pdf with flow data and the template.',
-	options: [
-		{
-			field: 'template',
-			name: 'Template',
-			type: 'string',
-			meta: {
-				width: 'full',
-				interface: 'input-code',
-			},
-		},
-
-	],
+    id: 'operation-pdf-generator',
+    name: 'Generate PDF',
+    icon: 'picture_as_pdf',
+    description: 'Generate a pdf with flow data and the template.',
+    overview: ({filename}) => [
+        {
+            label: 'Filename',
+            text: filename,
+        }
+    ],
+    options: [
+        {
+            field: 'permissions',
+            name: '$t:permissions',
+            type: 'string',
+            schema: {
+                default_value: '$trigger',
+            },
+            meta: {
+                width: 'full',
+                interface: 'select-dropdown',
+                options: {
+                    choices: [
+                        {
+                            text: 'From Trigger',
+                            value: '$trigger',
+                        },
+                        {
+                            text: 'Public Role',
+                            value: '$public',
+                        },
+                        {
+                            text: 'Full Access',
+                            value: '$full',
+                        },
+                    ],
+                    allowOther: true,
+                },
+            },
+        },
+        {
+            field: 'filename',
+            name: 'Filename',
+            type: 'string',
+            meta: {
+                width: 'full',
+                interface: 'input',
+            }
+        },
+        {
+            field: 'folder',
+            name: 'Folder',
+            type: 'uuid',
+            meta: {
+                width: 'half',
+                interface: 'system-folder',
+            }
+        },
+        {
+            field: 'storage',
+            name: 'Storage',
+            type: 'string',
+            meta: {
+                width: 'half',
+                interface: 'input',
+                options: {
+                    placeholder: 'local',
+                    iconRight: 'storage',
+                }
+            }
+        },
+        {
+            field: 'template',
+            name: 'Template',
+            type: 'json',
+            meta: {
+                width: 'full',
+                interface: 'input-code',
+                options: {
+                    lineWrapping: true,
+                    language: 'JSON',
+                    template: JSON.stringify({"content": []}),
+                }
+            },
+        },
+        {
+            field: 'fonts',
+            name: 'Fonts',
+            type: 'json',
+            meta: {
+                width: 'full',
+                interface: 'input-code',
+                options: {
+                    lineWrapping: true,
+                    language: 'JSON',
+                    template: JSON.stringify({
+                        "FontName-Regular.ttf": "",
+                        "FontName-Italic.ttf": "",
+                        "FontName-Bold.ttf": "",
+                        "FontName-BoldItalic.ttf": "",
+                    }),
+                }
+            }
+        }
+    ],
 };
