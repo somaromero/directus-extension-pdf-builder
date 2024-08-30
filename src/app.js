3746b7515b887abd463288d22ae510691feff6fc
Somar Romero <somar.dev@icloud.com>
2024-08-30 15:03:45 -0700

    Rename operation name :art:

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 export default {
     id: 'operation-pdf-builder',
-    name: 'PDF Builder',
+    name: 'PDF Builder Operation',
     icon: 'picture_as_pdf',
     description: 'Generate a pdf with flow data and the template.',
     overview: ({filename}) => [
